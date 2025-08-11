20587c5bf130afc572cd292ae12517b84cbf71b7
Victor <victor2002gabriel@gmail.com>
2025-08-11 14:27:16 -0300

    Implementação de autenticação JWT e permissões de usuarios

--- src/main/java/br/com/quizmaster/quiz/repository/UsuarioRepository.java
+++ src/main/java/br/com/quizmaster/quiz/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import br.com.quizmaster.quiz.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
     boolean existsByLoginEmail(String loginEmail);
+
+    Optional<UsuarioModel> findByLoginEmail(String loginEmail);
 }
---
+++ src/main/java/br/com/quizmaster/quiz/rest/dto/LoginRequestDTO.java
@@ -0,0 +1,2 @@
+package br.com.quizmaster.quiz.rest.dto;
+public record LoginRequestDTO(String email, String senha) {}
